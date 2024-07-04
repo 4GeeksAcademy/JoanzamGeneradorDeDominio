abd3b1ba6b70c46b240adf2ce64941d25bdeab7b
Joanzam <joanzam.com@gmail.com>
2024-07-04 16:22:30 +0000

    ejer completado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorDominio() {
+    let resultado;
+    let pronouns = ["that", "my"];
+    let adjs = ["small", "bab"];
+    let nouns = ["sea", "school"];
+    let dominios = [];
+
+    pronouns.forEach((pronouns) => {
+      adjs.forEach((adjs) => {
+        nouns.forEach((nouns) => {
+          let dominio = pronouns + adjs + "com";
+          dominios.push(dominio);
+        });
+      });
+    });
+    return dominios;
+  }
+  let dominioTotal = generadorDominio();
+  console.lot(dominioTotal);
+  resultado = document.getElementById("dominio").innerHTML = resultado;
+  pronouns.forEach((pronouns) => {});
 };
